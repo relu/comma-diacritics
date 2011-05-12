03416221de41e1d81f1635f337c5ca5178c30e34
Aurel Canciu <aurelcanciu@gmail.com>
2011-05-12 23:46:54 +0300

    Added some minor modifications for IE6

--- js/comma_dias_check.js
+++ js/comma_dias_check.js
@@ -1,5 +1,14 @@
-jQuery(document).ready(function() {  
-    if( navigator.platform == "Win32" || navigator.platform == "Win64" ) {
+function msieversion() {
+	var ua = window.navigator.userAgent
+	var msie = ua.indexOf ( "MSIE " )
+	if ( msie > 0 )
+		return parseInt ( ua.substring ( msie+5, ua.indexOf ( ".", msie ) ) )
+	else
+		return 0
+}
+
+jQuery(document).ready(function() {
+    if ( msieversion() <= 6 && msieversion() != 0 ) {
         jQuery('#wrapper').commaDias();
         jQuery('title').commaDias();
     }
